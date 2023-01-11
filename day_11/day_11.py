ef6881613e013f7e500f0ee51173f72c1c14b211
Colin McNeece <colin.mcneece@c02fm930ml85.attlocal.net>
2023-01-10 21:11:44 -0600

    Get it working in module form

---
+++ day_11/day_11.py
@@ -0,0 +1,119 @@
+from dataclasses import dataclass
+from math import floor, prod
+import copy
+
+
+@dataclass
+class Monkey:
+    """ Monkey item."""
+    id: int
+    items: list[int]
+    operation: tuple[callable, int]
+    test_factor: int
+    true_monkey: int
+    false_monkey: int
+    items_inspected: int = 0
+
+    def test_function(self, item):
+        return (item % self.test_factor) == 0
+
+    def inspect(self, monkeys, part, supermodulo):
+        for item in self.items:
+            old = item
+            # old evaluated in here
+            new = eval(self.operation)
+            if part == 1:
+                new = floor(new / 3)
+            elif part == 2:
+                new %= supermodulo
+                # print(MONKEY_DIVISOR_CONSTANT)
+
+            destination_monkey = self.true_monkey if self.test_function(new) else self.false_monkey
+            monkeys[destination_monkey].items.append(new)
+            self.items_inspected += 1
+
+        self.items = []
+
+        return monkeys
+
+
+def parse_input(input_path):
+
+    with open(input_path, 'r') as f:
+        data = f.readlines()
+
+    block_start = range(0, len(data), 7)
+    block_stop = range(6, len(data)+1, 7)
+
+    blocks = []
+    for start, stop in zip(block_start, block_stop):
+        blocks.append(data[start:stop])
+
+    monkeys = {}
+    supermodulo = 1
+    for block in blocks:
+        for j, _ in enumerate(block):
+
+            # remove all spaces and \n
+            line = block[j].strip()
+
+            if j == 0:
+                # monkey id
+                line = line.strip(":")
+                id = int(line.split()[1])
+            elif j == 1:
+                # current items
+                items_text = line.split(":")[1].split(",")
+                items = []
+                for entry in items_text:
+                    items.append(int(entry.strip()))
+            elif j == 2:
+                # operation
+                operation = line.split(":")[1].strip(",").replace(" ", "").replace("new=", "")
+            elif j == 3:
+                test_factor = int(line.replace("Test: divisible by", ""))
+                supermodulo *= test_factor
+            elif j == 4:
+                true_monkey = int(line.replace("If true: throw to monkey ", ""))
+            elif j == 5:
+                false_monkey = int(line.replace("If false: throw to monkey ", ""))
+
+        monkey = Monkey(id=id, items=items, operation=operation, test_factor=test_factor,
+                        true_monkey=true_monkey, false_monkey=false_monkey)
+
+        monkeys[id] = monkey
+
+    return monkeys, supermodulo
+
+
+def execute_rounds(monkeys, rounds, part, supermodulo):
+    for _ in range(rounds):
+        for id in range(len(monkeys)):
+            monkeys = monkeys[id].inspect(monkeys=monkeys, part=part,
+                                          supermodulo=supermodulo)
+
+    items_inspected = []
+    for _, monkey in monkeys.items():
+        items_inspected.append(monkey.items_inspected)
+
+    items_inspected.sort()
+
+    monkey_business = prod(items_inspected[-2:])
+    return monkey_business
+
+
+if __name__ == "__main__":
+    INPUT_PATH = 'input.txt'
+    MSG = "Part {part} solution is {solution}"
+
+    monkeys, supermodulo = parse_input(INPUT_PATH)
+    part_1_monkeys = copy.deepcopy(monkeys)
+    part_2_monkeys = copy.deepcopy(monkeys)
+
+    part_1_solution = execute_rounds(monkeys=part_1_monkeys, rounds=20, part=1,
+                                     supermodulo=supermodulo)
+    print(MSG.format(part=1, solution=part_1_solution))
+
+    part_2_solution = execute_rounds(monkeys=part_2_monkeys, rounds=10000, part=2,
+                                     supermodulo=supermodulo)
+    print(MSG.format(part=2, solution=part_2_solution))
