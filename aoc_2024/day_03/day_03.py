975be0f03548b63cbd0404c6ee1d2b6c3ce9f9c3
Colin McNeece <colinjmcneece@gmail.com>
2024-12-17 20:35:32 -0600

    transfer to py script

--- aoc_2024/day_03/day_03.py
+++ aoc_2024/day_03/day_03.py
@@ -5,22 +5,66 @@ import re
 
 DIR_PATH = pathlib.Path(__file__).parent.resolve()
 
+MUL_PATTERN = "mul\(\d{1,3},\d{1,3}\)"
+DIGIT_PATTERN = "\d{1,3}"
+DONT_PATTERN = "don\'t\(\)"
+DO_PATTERN = "do\(\)"
 
-def find_valid_muls(input):
-    mul_pattern = "\bmul\b\(\d{1,3},\d{1,3}\)"
-    matches = re.findall(mul_pattern, input)
+FULL_INSTRUCTION_PATTERN = "|".join([MUL_PATTERN, DONT_PATTERN, DO_PATTERN])
 
+
+def parse_memory_muls_only(input):
+    matches = re.findall(MUL_PATTERN, input)
     return matches
 
 
-def part_1(reports):
+def parse_memory_all(input):
+    matches = re.findall(FULL_INSTRUCTION_PATTERN, input)
+    return matches
 
-    return None
+
+def proccess_memory(memory):
+    instructions = []
+    for match in memory:
+        pairs = re.findall(DIGIT_PATTERN, match)
+        if pairs:
+            instructions.append((int(pairs[0]), int(pairs[1])))
+        else:
+            instructions.append(match)
+
+    return instructions
 
 
-def part_2(reports):
+def execute_instructions(instructions):
+    cum_sum = 0
+    do = True
+    for instruction in instructions:
+        if instruction == "don't()":
+            do = False
+            continue
+        if instruction == "do()":
+            do = True
+            continue
+        if do:
+            cum_sum += instruction[0] * instruction[1]
 
-    return None
+    return cum_sum
+
+
+def part_1(input):
+    memory = parse_memory_muls_only(input=input)
+    instructions = proccess_memory(memory=memory)
+    cum_sum = execute_instructions(instructions=instructions)
+
+    return cum_sum
+
+
+def part_2(input):
+    memory = parse_memory_all(input=input)
+    instructions = proccess_memory(memory=memory)
+    cum_sum = execute_instructions(instructions=instructions)
+
+    return cum_sum
 
 
 if __name__ == "__main__":
@@ -29,15 +73,14 @@ if __name__ == "__main__":
     INPUT_PATH = os.path.join(DIR_PATH, INPUT_FILE)
 
     with open(INPUT_PATH, 'r') as f:
-        data = f.read().splitlines()
-
+        input = f.read()
 
     # Part 1
-    part_1_answer = part_1(data)
-    part_1_msg = f"For Part 1 there are {part_1_answer} safe reports."
+    part_1_answer = part_1(input)
+    part_1_msg = f"You get {part_1_answer} if you add up all of the results of the multiplications."
     print(part_1_msg)
 
     # Part 2
-    part_2_answer = part_2()
-    part_2_msg = f"For Part 2 there are {part_2_answer} safe reports."
+    part_2_answer = part_2(input)
+    part_2_msg = f"You get {part_2_answer} if you add up all of the results of the multiplications."
     print(part_2_msg)
